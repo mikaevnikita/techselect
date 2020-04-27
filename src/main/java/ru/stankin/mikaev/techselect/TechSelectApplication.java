724999f152e37c76a0ebbae1be8b7a7df0b4df29
Никита Микаев <nmikaev@okta-solutions.com>
2020-04-27 18:45:34 +0300

    Initial commit

---
+++ src/main/java/ru/stankin/mikaev/techselect/TechSelectApplication.java
@@ -0,0 +1,13 @@
+package ru.stankin.mikaev.techselect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechSelectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechSelectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/stankin/mikaev/techselect/TechSelectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.stankin.mikaev.techselect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechSelectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
