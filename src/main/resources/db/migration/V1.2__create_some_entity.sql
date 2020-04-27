757c5fe98eff1dd49461d546593c4a36f3dad39d
Никита Микаев <nmikaev@okta-solutions.com>
2020-04-28 00:50:18 +0300

    Лишнее

--- src/main/resources/db/migration/V1.2__create_some_entity.sql
+++
@@ -1,8 +0,0 @@
--- Step #2: Creating table techselect.some_entity
-
-create table if not exists techselect.some_entity
-(
-    id bigint not null,
-    data varchar(128) not null,
-    primary key (id)
-);
